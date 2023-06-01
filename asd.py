827c9d47bd9a3c0e9698ad20b7bdc0caf5bb2ec5
tubajoskaa <133607816+tubajoskaa@users.noreply.github.com>
2023-06-01 16:59:54 +0200

    06.01
    06.01

--- asd.py
+++ asd.py
@@ -25,8 +25,84 @@ with open('Nyilvantartaspy.txt','r', encoding='utf-8') as forrasfajl:
         del magasugras[0]
         list.append(dictionary)
 
+#Távolugrás
+max_tavolugras = 0
+min_tavolugras = 100
+osszeg_tavolugras = 0
 for i in list:
-    max = 0
-    if max < i['Súlydobás']:
-        max = i['Súlydobás']
-    print(max)
+    osszeg_tavolugras += i['Távolugrás']
+    if max_tavolugras < i['Távolugrás']:
+        max_tavolugras = i['Távolugrás']
+    if min_tavolugras > i['Távolugrás']:
+        min_tavolugras = i['Távolugrás']
+print(f'A legjobb távolugrás: {max_tavolugras}')
+print(f'A legrosszabb távolugrás: {min_tavolugras}')
+print(f'Az összesen ugrott táv: {osszeg_tavolugras}')
+print(f'Az ugrott távolugrás átlaga: {osszeg_tavolugras/len(list)}')
+print('\n')
+
+#Helyből távolugrás
+max_helyboltavol = 0
+min_helyboltavol = 100
+osszeg_helyboltavol = 0
+for i in list:
+    osszeg_helyboltavol += i['Helyből_Távolugrás']
+    if max_helyboltavol < i['Helyből_Távolugrás']:
+        max_helyboltavol = i['Helyből_Távolugrás']
+    if min_helyboltavol > i['Helyből_Távolugrás']:
+        min_helyboltavol = i['Helyből_Távolugrás']
+print(f'A legjobb helyből távolugrás: {max_helyboltavol}')
+print(f'A legrosszabb helyből távolugrás: {min_helyboltavol}')
+print(f'Az összesen helyből távolugrott  táv: {osszeg_helyboltavol}')
+print(f'Az ugrott helyből távolugrás átlaga: {osszeg_helyboltavol/len(list)}')
+print('\n')
+
+#Súlydobás
+max_sulydobas = 0
+min_sulydobas = 100
+osszeg_sulydobas = 0
+for i in list:
+    osszeg_sulydobas += i['Súlydobás']
+    if max_sulydobas < i['Súlydobás']:
+        max_sulydobas = i['Súlydobás']
+    if min_sulydobas > i['Súlydobás']:
+        min_sulydobas = i['Súlydobás']
+print(f'A legjobb súlydobás: {max_sulydobas}')
+print(f'A legrosszabb súlydobás: {min_sulydobas}')
+print(f'Az összesen dobott táv: {osszeg_sulydobas}')
+print(f'Az dobott súlydobások átlaga: {osszeg_sulydobas/len(list)}')
+print('\n')
+
+
+#Futás
+max_futas = 0
+min_futas = 100
+osszeg_futas = 0
+for i in list:
+    osszeg_futas += i['Futás']
+    if max_futas < i['Futás']:
+        max_futas = i['Futás']
+    if min_futas > i['Futás']:
+        min_futas = i['Futás']
+print(f'A legjobb futás: {max_futas}')
+print(f'A legrosszabb futás: {min_futas}')
+print(f'Az összesen futott táv: {osszeg_futas}')
+print(f'Az futott távok átlaga: {osszeg_futas/len(list)}')
+print('\n')
+
+
+#Magasugrás
+max_magasugras = 0
+min_magasugras = 100
+osszeg_magasugras = 0
+for i in list:
+    osszeg_magasugras += i['Magasugrás']
+    if max_magasugras < i['Magasugrás']:
+        max_magasugras = i['Magasugrás']
+    if min_magasugras > i['Magasugrás']:
+        min_magasugras = i['Magasugrás']
+print(f'A legjobb magasugrás: {max_magasugras}')
+print(f'A legrosszabb magasugrás: {min_magasugras}')
+print(f'Az összesen ugrott táv: {osszeg_sulydobas}')
+print(f'Az ugrott magasugrás átlaga: {osszeg_sulydobas/len(list)}')
+print('\n')
